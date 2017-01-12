13d8ee564e7925d7c331e3fd00f3e150f8711367
Sasho <sash239@gmail.com>
2017-01-12 21:24:12 +0100

    add website

---
+++ js/script.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function(){
+  let menu = document.querySelector('.main-nav');
+  let goTop = document.getElementById('goTop');
+  let a = document.querySelectorAll('.main-nav li a');
+
+  setInterval(function(){
+    if (document.body.scrollTop > 0) {
+      menu.classList.add('shrink');
+      goTop.classList.add('display-goTop');
+      a.forEach(function(element) {
+        element.classList.add('link-color');
+      }, this);
+    }
+    else {
+      menu.classList.remove('shrink');
+      goTop.classList.remove('display-goTop');
+      a.forEach(function(element) {
+      element.classList.remove('link-color');
+      }, this);
+    }
+  }, 200);
+
+  let hamburger = document.getElementById('hamburger-menu');
+  let hamburgerImageElement = '<img class="hamburger-open" src="img/hamburger.svg" alt="Open Hamburger">';
+  let closeHamburgerImageElement = '<img src="img/close.svg" alt="Close Hamburger">';
+  hamburger.addEventListener('click', function(event){
+    if (event.target.classList.contains('hamburger-open')) {
+      menu.classList.add('show-nav');
+      event.target.outerHTML = closeHamburgerImageElement;
+    }
+    else {
+      menu.classList.remove('show-nav');
+      event.target.outerHTML = hamburgerImageElement;
+    }
+    
+  });
+});
